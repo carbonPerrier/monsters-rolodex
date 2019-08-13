39b357b1583cd134d9e4b20fc7051fd1970844b6
Kevin Dai <carbonPerrier2019@gmail.com>
2019-08-13 15:45:28 -0700

    Updates

---
+++ precache-manifest.cc49d094ef7ecebff0dcf7fababdee4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e80b685f9fd4d88076d33ff04121066",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "59a72ef58203fcc52934",
+    "url": "/monsters-rolodex/static/css/main.8733de87.chunk.css"
+  },
+  {
+    "revision": "82a854a45369142e9676",
+    "url": "/monsters-rolodex/static/js/2.b358fe6d.chunk.js"
+  },
+  {
+    "revision": "59a72ef58203fcc52934",
+    "url": "/monsters-rolodex/static/js/main.d02cd570.chunk.js"
+  },
+  {
+    "revision": "e22dc9ee982c5235665b",
+    "url": "/monsters-rolodex/static/js/runtime~main.93f27e09.js"
+  }
+]);
